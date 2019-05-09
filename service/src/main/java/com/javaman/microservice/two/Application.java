a4d275f78b747d37e71010f469e9f281881525c7
Tyrion <zhe.peng@lazada.com>
2019-05-10 00:10:13 +0800

    integration dubbo&nacos

--- service/src/main/java/com/javaman/microservice/two/Application.java
+++ service/src/main/java/com/javaman/microservice/two/Application.java
@@ -1,5 +1,6 @@
 package com.javaman.microservice.two;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableDubbo
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
---
+++ service/src/main/java/com/javaman/microservice/two/service/ApiServiceImpl.java
@@ -0,0 +1,30 @@
+package com.javaman.microservice.two.service;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.javaman.microservice.api.service.MicroService;
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * @author pengzhe
+ * @date 2019-05-09 23:01
+ * @description
+ */
+
+@Service(version = "${dubbo.service.version}")
+public class ApiServiceImpl implements MicroService {
+
+    @Value("${dubbo.service.name}")
+    private String serviceName;
+
+    @Override
+    public String fetchInfo(int i) {
+        RpcContext rpcContext = RpcContext.getContext();
+        return String.format("Service [name :%s , port : %d] %s(\"%s\") : Hello,%s",
+                serviceName,
+                rpcContext.getLocalPort(),
+                rpcContext.getMethodName(),
+                String.valueOf(i),
+                String.valueOf(i));
+    }
+}
